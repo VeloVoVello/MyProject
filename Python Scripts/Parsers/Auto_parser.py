9cc8af09555b080ebf1ff2301dc17b6d33ae4ba0
VeloVoVello <twintip.insnow@gmail.com>
2018-07-19 18:01:51 +0300

    Add .txt files

--- Python Scripts/Parsers/Auto_parser.py
+++ Python Scripts/Parsers/Auto_parser.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,11 +8,32 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
+filename = 'auto.csv'
+f = open(filename, 'a')
+
+headers = 'brand, price\n'
+
+f.write(headers)
+
+
 # grab ID
-soup.find_all(id="i1581458031")
+# soup.find_all(id="i1581458031")
 # grab Titles
 titles = soup.find_all(class_="item-description-title-link")
+title[0]['title'].replace(',', '')
+title[0]['title'].split(',')[0]
 # grab Price
-price = soup.find_all(class_="price")
+prices = soup.find_all(class_="price")
+price[0].text.strip()
 
+for i in titles:
+    title = i['title'].replace(',', '')
+    title_list.append(title)
+    print(title)
 
+for i in prices:
+    pri_1 = i.text.strip().replace('  ₽', '')
+    price = int(pri_1.replace(' ', ''))
+    print(price)
+
+f.close()
